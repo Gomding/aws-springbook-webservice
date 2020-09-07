c7578ad8da016b32b620be55837d698571a9cc4d
Gomding <backchida@gmail.com>
2020-09-08 02:45:04 +0900

    판매에 배송비 input 추가

--- src/main/java/com/book/spring/park/config/auth/LoginUser.java
+++ src/main/java/com/book/spring/park/config/auth/LoginUser.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.PARAMETER)
-@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER) // 어노테이션이 생성될 수 있는 위치를 지정
+@Retention(RetentionPolicy.RUNTIME) // 어느 시점까지 어노테이션이 영향을 미칠지 결정함
 public @interface LoginUser {
 }
